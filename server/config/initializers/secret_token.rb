d08605e5df3965ed0f027be09a9f895822542ca6
Channely <qijie29896@gmail.com>
2013-10-13 10:53:57 +0800

    add all base code

---
+++ server/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Server::Application.config.secret_key_base = '1c4bf7b79bb9ef153db05f56f4b37019defcd18c18e9488ff3e2d74de1e292cd3a3308431698bac410e1d2e4b46650a2081b86431e3e40323b0778cc90e39057'
